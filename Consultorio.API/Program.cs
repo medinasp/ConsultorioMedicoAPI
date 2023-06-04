48c6274360e1dafda6f715303bda0bd9211f8f42
medinasp <medinasp@gmail.com>
2023-06-04 10:05:34 -0300

    Initial Commit API

---
+++ Consultorio.API/Program.cs
@@ -0,0 +1,54 @@
+using ConsultorioMedico.Aplicacao;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+builder
+    .Services
+    .AddApplication();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "CadFuncAPI",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Eneias Medina",
+            Email = "medinasp@gmail.com",
+            Url = new Uri("https://github.com/medinasp")
+
+        }
+    });
+
+    var xmlFile = "Consultorio.API.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
